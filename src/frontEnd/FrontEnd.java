028552c05644d5d32ae025edb816d72704f2d293
Lodrak <peter.nilsson.1991@hotmail.com>
2018-02-27 13:02:24 +0100

    Created sockets and received all left is to decide to read or to send

--- src/frontEnd/FrontEnd.java
+++ src/frontEnd/FrontEnd.java
@@ -1,37 +1,61 @@
 package frontEnd;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class FrontEnd {
     private boolean mNoPrimary;
-    //private ? mPrimary;
+    private DatagramSocket mPrimary;
+    private DatagramSocket mSocket;
 
     public static void main(String[] args) {//starts constructor and do a handshake
         if (args.length < 1) {
             System.err.println("Usage: java FrontEnd portnumber");
             System.exit(-1);
         }
+        FrontEnd instance = null;
         try {
-            FrontEnd instance = new FrontEnd(Integer.parseInt(args[0]));
-            instance.listenAndSend();
+            instance = new FrontEnd(Integer.parseInt(args[0]));
         } catch (NumberFormatException e) {
             System.err.println("Error: port number must be an integer.");
             System.exit(-1);
         }
+        instance.listenAndSend();
     }
     private FrontEnd(int portNumber) {//initialize variables
-            mNoPrimary = true;
-            //socket = f(portNumber);
+    	mNoPrimary = true;
+    	try {
+            mSocket = new DatagramSocket(portNumber);
+    	} catch (Exception e) {
+    		e.printStackTrace(); 
+    		System.exit(-1);
+    	}
     }
 
     private void listenAndSend() {
         System.out.println("Waiting for handshake...!");
         do {
-            //? recieved = socket.recieve();
-            //if(recieved.tell()) readPrimary();
-            //else mPrimary.send(recieved);
+    		byte[] buf = new byte[256];
+    		DatagramPacket received = new DatagramPacket(buf, buf.length);
+            try {
+            	mSocket.receive(received);
+            } catch (Exception e) {
+            	e.printStackTrace(); 
+            	System.exit(-1);
+            }
+            if(!readPrimary(received)) send(received);
         } while (true);
     }
     
-    private void readPrimary(){ 
+    private boolean readPrimary(DatagramPacket received){
+    	//if msg says readprimary do it
     	//read from file and set mPrimary to it
+    	//return true
+    	return false;
     }
+    private void send(DatagramPacket received) {
+    	//send received to the address it points to
+    	//or if it asks to be sent to primary send it to primary
+    }
+    
 }
