9d4240df9d2ee9491631f83aab80c5859d02b2b7
Lodrak <peter.nilsson.1991@hotmail.com>
2018-03-01 09:15:40 +0100

    Added Communication Information

--- src/both/Message.java
+++ src/both/Message.java
@@ -1,14 +1,23 @@
 package both;
 
 import java.io.Serializable;
+import java.net.InetAddress;
 import java.util.LinkedList;
 
 public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private LinkedList<GObject> objectList ;
+	private	final boolean mToPrimary;
+	private final InetAddress mClient;
+	private final int mPort;
+	
 
-	public Message(LinkedList<GObject> objectList) {
+	public Message(LinkedList<GObject> objectList, boolean toPrimary, InetAddress lClient, int Port) {
 		super();
 		this.objectList = objectList;
+		mToPrimary = toPrimary;
+		mClient = lClient;
+		mPort = Port;
 	}
 
 	public LinkedList<GObject> getObjectList() {
@@ -18,5 +27,16 @@ public class Message implements Serializable {
 	public void setObjectList(LinkedList<GObject> objectList) {
 		this.objectList = objectList;
 	}
-
+	
+	public boolean getToPrimary() {
+		return mToPrimary;
+	}
+	
+	public InetAddress getClient() {
+		return mClient;
+	}
+	
+	public int getPort() {
+		return mPort;
+	}
 }
