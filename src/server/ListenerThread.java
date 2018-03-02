39e8677ad96a1cd9fe9d95cffb7c8922f2e3cc82
CallMeMajorTom <32645163+CallMeMajorTom@users.noreply.github.com>
2018-03-02 15:23:09 +0800

    Modify the frame code of Client and Server to fit the model(didn't finish)

--- src/server/ListenerThread.java
+++ src/server/ListenerThread.java
@@ -1,37 +1,51 @@
 package server;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
 
 import both.Message;
 
 //responsible for receiving messages from the client
 public class ListenerThread extends Thread {
 	Server server = null;
-	ClientConnection cc;
-	ObjectInputStream r;
+	ClientConnection m_ClientConnection;
 
 	// Construtor
-	public ListenerThread(Server server, ClientConnection cc) {
+	public ListenerThread(Server server, ClientConnection m_ClientConnection) {
 		this.server = server;
-		this.cc = cc;
-		r = cc.getObjectInputStream();
+		this.m_ClientConnection = m_ClientConnection;
 	}
 
 	// keep receving messages until the socket is closed
 	public void run() {
 		while (true) {
 			Message message = null;
+			byte[] incomingData = new byte[256];
+			DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+			ByteArrayInputStream byte_stream =new ByteArrayInputStream(incomingPacket.getData());
+			ObjectInputStream object_stream = null;
 			try {
-				message = (Message) r.readObject();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
+				object_stream = new ObjectInputStream(byte_stream);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			try {
+				message = (Message)object_stream.readObject();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
 			System.out.println("message recevied" + message);
 			// call method to process the message
-			server.broadcast(message);
+			try {
+				server.broadcast(message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,27 +1,62 @@
 package server;
 
-import java.net.Socket;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import both.Message;
+
+import javax.print.attribute.standard.MediaSize;
 
 public class Server {
 	// an array list of Clients
 	private ArrayList<ClientConnection> connectedClients = new ArrayList<ClientConnection>();
+	private ArrayList Other_Server_Port = new ArrayList();
+	private final int FEPort;
+	private final int Port;
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.err.println("Usage: java Server portnumber");
+			System.exit(-1);
+		}
+		int Port = Integer.parseInt(args[0]);
+		int FEPort = Integer.parseInt(args[1]);
+		ArrayList Server_Port = new ArrayList();
+		for(int i = 2; i < args.length; i++)
+			Server_Port.add(Integer.parseInt(args[i]));
+
+		Server instance = new Server(Port,FEPort,Server_Port);
+	}
+
+	public Server(int m_Port, int m_FEPort, ArrayList Server_Port) {
+			Port = m_Port;
+			FEPort = m_FEPort;
+			Other_Server_Port = Server_Port;
+	}
+
+
+	private void listenForClientMessages() {
+
+		System.out.println("Waiting for client messages... ");
+
+		do {
+		}
+		while(true);
+	}
 
 	// adds a client, returns false if name is already used
-	public synchronized boolean addClient(Socket socket) {
-		ClientConnection c;
-		ClientConnection cc = new ClientConnection(socket);
-		connectedClients.add(cc);
-		ListenerThread t = new ListenerThread(this, cc);
-		t.start();
+	public synchronized boolean addClient(String hostName, int port) throws SocketException, UnknownHostException {
+		ClientConnection m_ClientConnection = new ClientConnection(hostName,port);
+		connectedClients.add(m_ClientConnection);
+		ListenerThread receive_message = new ListenerThread(this, m_ClientConnection);
+		receive_message.start();
 		System.out.println("Client added");
 		return true;
 	}
 
-	public synchronized void broadcast(Message message) {
+	public synchronized void broadcast(Message message) throws IOException {
 		for (ClientConnection cc : connectedClients) {
 			cc.sendMessage(message);
 		}
