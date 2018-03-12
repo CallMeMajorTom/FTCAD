cfc8ed952b86b82662446f98c3e93203fbbd8209
Lodrak <peter.nilsson.1991@hotmail.com>
2018-03-12 17:18:29 +0100

    test rebase

--- src/server/Primary.java
+++ src/server/Primary.java
@@ -8,33 +8,73 @@ import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.util.ArrayList;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.ConversionException;
+import org.apache.commons.configuration.XMLConfiguration;
+
 public class Primary extends State{
 	
 	Server mServer = null;
 
     protected State update(Server server){
     	mServer = server;
-		//write to primary file
-		//tell frontend to read
+    	writeNtell();
     	while(true) {
-    		//updatedelay
-    		//stop producing
+    		Thread.sleep(50);
+    		stopProducing();
     		ArrayList<Message> msgs;
     		for (int i = server.mExpectedBQ.size(); 0<i ;i--) {
 	    		Message msg = new server.mExpectedBQ.take();
 	    		msg = Message(msg, server.mID);
-	    		server.mID++;
+	    		server.mMsgID++;
 	    		server.mMessageList.add(msg);
 	    		msgs.add(msg);
     		}
-    		//update backups
+    		updateBackups();
     		sendAcks();
-    		//start producing
-    		broadcast(msgs);//change to send each message in msgs to every client
+    		startProducing();
+    		broadcast(msgs);
     	}
     }
     
-    private void sendAcks(){
+	private void writeNtell() {
+		//TODO write to primary file and tell frontend to read
+		@SuppressWarnings("unused")
+		XMLConfiguration conf = null;
+		try {
+			conf = new XMLConfiguration("primary.xml");
+		} catch (ConfigurationException e) {
+			//TODO create primary file
+		}
+		//TODO change port to this servers port with help  of conf
+	} 
+	
+	private int readfile() {
+		XMLConfiguration conf = null;
+		try {
+			conf = new XMLConfiguration("primary.xml");
+			return conf.getInt("port");
+		} catch (ConfigurationException e) {
+			System.err.println("primary file doesnt exist");//if primaryfile doesnt exist
+		} catch (ConversionException e) {
+			System.err.println("not a port");//if port doesnt exist
+    	}
+    	return 0;
+    }
+
+	private void stopProducing() {
+    	//TODO stop producing
+	}
+
+	private void startProducing() {
+		//TODO start producing
+	}
+
+    private void updateBackups() {
+		// TODO update the backups with the latest updates
+	}
+    
+	private void sendAcks(){
         for (FEConnectionToClient cc : mServer.mFEConnectionToClients) {
             cc.sendAcks();
         }
