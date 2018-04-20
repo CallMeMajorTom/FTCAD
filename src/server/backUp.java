0e8ff7915cf43b7d849d95ff846d2fe29f3ab6a3
CallMeMajorTom <32645163+CallMeMajorTom@users.noreply.github.com>
2018-04-20 18:14:05 +0800

    Try to fix the update version.

--- src/server/backUp.java
+++ src/server/backUp.java
@@ -4,6 +4,7 @@ public class backUp extends State {
 	protected State update(Server server) {
 		System.out.println("backUp state");
 		if(!server.isPrimaryAlive()) return new Voting();//If the primary died now
+		server.askingForUpdate();
 		return this;//TODO implement this right
 	}
 }
